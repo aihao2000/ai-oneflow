110250eb8be1531bff666457f9407499cff8dd67
aihao <aihao@xiaohongshu.com>
2024-08-04 01:26:51 +0800

    add blip caption

---
+++ blip_caption.py
@@ -0,0 +1,102 @@
+import numpy as np
+import onnxruntime as rt
+import argparse
+from PIL import Image, ImageFile
+import os
+import huggingface_hub
+import pandas as pd
+import argparse
+from glob import glob
+from multiprocessing import Pool, current_process
+from tqdm import tqdm
+import json
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, default=".")
+    parser.add_argument("--num_processes", type=int, default=1)
+    parser.add_argument("--save_path", type=str, default=None)
+    parser.add_argument("--rel_path", type=str, default=None)
+    parser.add_argument("--model_path", type=str, default=None)
+    parser.add_argument("--num_gpus", type=int, default=1)
+    args = parser.parse_args()
+
+    if args.save_path is None:
+        args.save_path = os.path.join(args.dataset_path, "blip_captions.json")
+    else:
+        os.makedirs(os.path.dirname(args.save_path), exist_ok=True)
+
+    if args.rel_path is None:
+        args.rel_path = args.dataset_path
+
+    return args
+
+
+def gen_captions(image_path):
+    global model
+    global processor
+    image = Image.open(image_path)
+    inputs = processor(image, return_tensors="pt").to(model.device)
+    out = model.generate(**inputs)
+
+    return processor.decode(out[0], skip_special_tokens=True)
+
+
+def is_image(image_path):
+    image_types = ["png", "jpg", ".peg", "gif", "webp", "bmp", "jpeg"]
+    if image_path.split(".")[-1] not in image_types:
+        return False
+    # try:
+    #     Image.open(image_path).convert("RGBA")
+    # except Exception:
+    #     print(f"Error opening {image_path}")
+    #     return False
+    else:
+        return True
+
+
+def is_valid_image(image_path):
+    try:
+        Image.open(image_path).convert("RGBA")
+    except Exception:
+        print(f"Error opening {image_path}")
+        return False
+    else:
+        return True
+
+
+def init_subprocess(model_path, num_gpus):
+    global model
+    global processor
+    processor = BlipProcessor.from_pretrained(model_path)
+    model = BlipForConditionalGeneration.from_pretrained(model_path).to(
+        f"cuda:{(current_process()._identity[0] - 1) % num_gpus}"
+    )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    image_paths = glob(f"{args.dataset_path}/**", recursive=True)
+    image_paths = [image_path for image_path in image_paths if is_image(image_path)]
+
+    print(f"num images:{len(image_paths)}")
+    print("gen tags")
+    with Pool(
+        processes=args.num_processes,
+        initializer=init_subprocess,
+        initargs=(args.model_path, args.num_gpus),
+    ) as p:
+        results = list(tqdm(p.imap(gen_captions, image_paths), total=len(image_paths)))
+
+    prompts = {}
+
+    for image_path, prompt in zip(image_paths, results):
+        prompts[os.path.relpath(image_path, args.rel_path)] = prompt
+
+    with open(args.save_path, "w") as f:
+        json.dump(prompts, f, indent=4)
